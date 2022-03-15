9ecf051b711ddac4a58890c203893a86a881e465
Hoa <hoa301220@gmail.com>
2022-03-16 01:28:05 +0700

    install prettier husky lint-staged

--- src/app/controllers/NewsController.js
+++ src/app/controllers/NewsController.js
@@ -1,16 +1,14 @@
-
-
-class NewController{
+class NewController {
     // [GET] /news
 
-    index(req, res){
-        res.render('news')
+    index(req, res) {
+        res.render('news');
     }
-    
+
     // [GET] /: slug
-    show(req, res){
-        res.send('abcde')
+    show(req, res) {
+        res.send('abcde');
     }
 }
 
-module.exports = new NewController;
+module.exports = new NewController();
